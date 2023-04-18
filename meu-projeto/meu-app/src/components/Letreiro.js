44b8e31035587e26e1798b2fccec673d22a3a4fb
Yasmin Gabrielle <102554669+YasminGab@users.noreply.github.com>
2023-04-18 19:31:32 -0300

    update

--- meu-projeto/meu-app/src/components/Letreiro.js
+++ meu-projeto/meu-app/src/components/Letreiro.js
@@ -1,10 +1,33 @@
-export function Letreiro() {
+import React, { useState, useEffect } from "react";
+
+function Letreiro() {
+  const [texto, setTexto] = useState(""); //define uma variável de estado para o texto
+  const [indice, setIndice] = useState(0); //define uma variável para o índice da letra
+
+  useEffect(() => {
+    // .useEffect atualiza o texto do letreiro a cada 150ms até que a mensagem inteira tenha sido exibida
+    const mensagem = "Venha estudar na Fatec!";
+    if (indice < mensagem.length) {
+      const timer = setTimeout(() => {
+        setTexto((textoAnterior) => textoAnterior + mensagem[indice]); //adiciona a letra correspondente ao texto do letreiro
+        setIndice((indiceAnterior) => indiceAnterior + 1); //atualiza o índice
+      }, 150);
+      return () => clearTimeout(timer); //limpa o cronômetro para uma função de retorno de chamada que foi configurada com o método setTimeout()
+    }
+  }, [texto, indice]);
 
   return (
-    <h1>Letreiro</h1>
-
-    
+    <h1
+      style={{
+        color: "#9120bd",
+        fontFamily: "Roboto Mono",
+        textAlign: "center",
+        fontSize: "100px",
+      }}
+    >
+      {texto}
+    </h1>
   );
 }
 
-export default Letreiro
+export default Letreiro;
