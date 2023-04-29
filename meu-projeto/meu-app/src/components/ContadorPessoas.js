f3477d1a8377da97e8ac687f48a15ee2d52a5001
Yasmin Gabrielle <102554669+YasminGab@users.noreply.github.com>
2023-04-29 11:17:54 -0300

    css

--- meu-projeto/meu-app/src/components/ContadorPessoas.js
+++ meu-projeto/meu-app/src/components/ContadorPessoas.js
@@ -9,15 +9,15 @@ import remove from '../assets/imagens/remove.png'
 
 export default function App() {
 
-    const [homens, setHomens] = useState(0); //variável de estado para homens e mulheres
+    const [homens, setHomens] = useState(0); 
     const [mulheres, setMulheres] = useState(0);
 
-    const resetar = () => { //reseta as quantidades de homens e mulheres para zero
+    const resetar = () => { 
         setHomens(0);
         setMulheres(0);
     };
 
-    const incrementar = (genero) => { //incrementa a quantidade do gênero passado como parâmetro
+    const incrementar = (genero) => { 
         if (genero === 'H') {
             setHomens(homens + 1);
         } else if (genero === 'M') {
@@ -25,7 +25,7 @@ export default function App() {
         }
     };
 
-    const decrementar = (genero) => { //decrementa a quantidade do gênero passado como parâmetro
+    const decrementar = (genero) => { 
         if (genero === 'H' && homens > 0) {
             setHomens(homens - 1);
         } else if (genero === 'M' && mulheres > 0) {
